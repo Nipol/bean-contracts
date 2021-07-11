8e6ffc5d39899bc3ea8b946af8d8b323beef24af
yoonsung.eth <ys.choi@me.com>
2021-07-11 22:35:10 +0900

    fix: interfaces (#6)

--- contracts/interfaces/IBurn.sol
+++ contracts/interfaces/IBurn.sol
@@ -6,4 +6,6 @@ pragma solidity ^0.8.0;
 
 interface IBurn {
     function burn(uint256 value) external returns (bool);
+
+    function burnFrom(address from, uint256 value) external returns (bool);
 }
