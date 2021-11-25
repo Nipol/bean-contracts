a184d14f9724b5ea038336a386a782800b423cf9
yoonsung.eth <ys.choi@me.com>
2021-11-25 13:41:19 +0900

    remove only

--- test/ERC721.spec.ts
+++ test/ERC721.spec.ts
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import { ethers } from 'hardhat';
 import { Contract, BigNumber, constants, Signer } from 'ethers';
 
-describe.only('ERC721', () => {
+describe('ERC721', () => {
   let ERC721Mock: Contract;
 
   let wallet: Signer;
