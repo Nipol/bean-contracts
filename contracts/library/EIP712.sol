0f9e5a976002b9f7a6bcd093fbcd739ac5a7aec7
yoonsung <ys.choi@me.com>
2022-03-22 22:44:52 +0900

    EIP721 with eth_signTypedData_v4

--- contracts/library/EIP712.sol
+++ contracts/library/EIP712.sol
@@ -41,6 +41,41 @@ library EIP712 {
     }
 
     /**
+     * @dev Calculates a EIP712 domain separator.
+     * @param name The EIP712 domain name.
+     * @param version The EIP712 domain version.
+     * @param verifyingContract The EIP712 verifying contract.
+     * @param salt The EIP712 verifying contract.
+     * @return result EIP712 domain separator.
+     */
+    function hashDomainSeperator(
+        string memory name,
+        string memory version,
+        address verifyingContract,
+        bytes32 salt
+    ) internal view returns (bytes32 result) {
+        bytes32 typehash = EIP712DOMAIN_TYPEHASH;
+
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            let nameHash := keccak256(add(name, 32), mload(name))
+            let versionHash := keccak256(add(version, 32), mload(version))
+            let chainId := chainid()
+
+            let memPtr := mload(64)
+
+            mstore(memPtr, typehash)
+            mstore(add(memPtr, 32), nameHash)
+            mstore(add(memPtr, 64), versionHash)
+            mstore(add(memPtr, 96), chainId)
+            mstore(add(memPtr, 128), verifyingContract)
+            mstore(add(memPtr, 160), salt)
+
+            result := keccak256(memPtr, 192)
+        }
+    }
+
+    /**
      * @dev Calculates EIP712 encoding for a hash struct with a given domain hash.
      * @param domainHash Hash of the domain domain separator data, computed with getDomainHash().
      * @param hashStruct The EIP712 hash struct.
---
+++ contracts/mocks/EIP712Mock.sol
@@ -0,0 +1,155 @@
+/**
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+pragma solidity ^0.8.0;
+
+import "../library/EIP712.sol";
+import "hardhat/console.sol";
+
+error InvalidSignature(address recovered);
+
+contract EIP712Mock {
+    struct Value {
+        uint256 value;
+    }
+
+    struct Receiver {
+        uint256[] value;
+    }
+
+    struct Receive {
+        address[] receivers;
+        uint256[] values;
+    }
+
+    string public constant name = "EIP712Mock";
+    string public constant version = "1";
+    bytes32 public immutable DOMAIN_SEPARATOR;
+    bytes32 public constant VERIFY_PLAIN_TYPEHASH = keccak256("Verify(address owner,uint256 value,uint256 nonce)");
+    bytes32 public constant VERIFY_ARRAY_TYPEHASH = keccak256("Verify(address owner,uint256[] values,uint256 nonce)");
+
+    bytes32 public constant VALUE_TYPEHASH = keccak256("Value(uint256 value)");
+    // Custom Types used must be located at the end.
+    bytes32 public constant VERIFY_COMPLEX_TYPEHASH =
+        keccak256("Verify(address owner,Value value,uint256 nonce)Value(uint256 value)");
+
+    bytes32 public constant RECEIVER_TYPEHASH = keccak256("Receiver(uint256[] value)");
+    bytes32 public constant VERIFY_VERYCOMPLEX_TYPEHASH =
+        keccak256("Verify(address owner,Receiver value,uint256 nonce)Receiver(uint256[] value)");
+
+    bytes32 public constant RECEIVE_TYPEHASH = keccak256("Receive(address[] receivers,uint256[] values)");
+    bytes32 public constant VERIFY_VERYVERYCOMPLEX_TYPEHASH =
+        keccak256("Verify(address owner,Receive value,uint256 nonce)Receive(address[] receivers,uint256[] values)");
+
+    mapping(address => uint256) public nonces;
+
+    constructor(bytes32 salt) {
+        DOMAIN_SEPARATOR = salt == bytes32(0)
+            ? EIP712.hashDomainSeperator(name, version, address(this))
+            : EIP712.hashDomainSeperator(name, version, address(this), salt);
+    }
+
+    function verify(
+        address owner,
+        uint256 value,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+        uint256 nonce = nonces[owner]++;
+        bytes32 digest = EIP712.hashMessage(
+            DOMAIN_SEPARATOR,
+            keccak256(abi.encode(VERIFY_PLAIN_TYPEHASH, owner, value, nonce))
+        );
+
+        address recovered = ecrecover(digest, v, r, s);
+        if (recovered == address(0) || recovered != owner) revert InvalidSignature(recovered);
+    }
+
+    function verify(
+        address owner,
+        uint256[] calldata values,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+        uint256 nonce = nonces[owner]++;
+        bytes32 digest = EIP712.hashMessage(
+            DOMAIN_SEPARATOR,
+            keccak256(abi.encode(VERIFY_ARRAY_TYPEHASH, owner, keccak256(abi.encodePacked(values)), nonce))
+        );
+
+        address recovered = ecrecover(digest, v, r, s);
+        if (recovered == address(0) || recovered != owner) revert InvalidSignature(recovered);
+    }
+
+    function verify(
+        address owner,
+        Value calldata va,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+        uint256 nonce = nonces[owner]++;
+        bytes32 digest = EIP712.hashMessage(
+            DOMAIN_SEPARATOR,
+            keccak256(abi.encode(VERIFY_COMPLEX_TYPEHASH, owner, hashStruct(va), nonce))
+        );
+
+        address recovered = ecrecover(digest, v, r, s);
+        if (recovered == address(0) || recovered != owner) revert InvalidSignature(recovered);
+    }
+
+    function hashStruct(Value calldata value) public pure returns (bytes32) {
+        return keccak256(abi.encode(VALUE_TYPEHASH, value.value));
+    }
+
+    function verify(
+        address owner,
+        Receiver calldata va,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+        uint256 nonce = nonces[owner]++;
+        bytes32 digest = EIP712.hashMessage(
+            DOMAIN_SEPARATOR,
+            keccak256(abi.encode(VERIFY_VERYCOMPLEX_TYPEHASH, owner, hashStruct(va), nonce))
+        );
+
+        address recovered = ecrecover(digest, v, r, s);
+        if (recovered == address(0) || recovered != owner) revert InvalidSignature(recovered);
+    }
+
+    function hashStruct(Receiver calldata value) public pure returns (bytes32) {
+        return keccak256(abi.encode(RECEIVER_TYPEHASH, keccak256(abi.encodePacked(value.value))));
+    }
+
+    function verify(
+        address owner,
+        Receive calldata va,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+        uint256 nonce = nonces[owner]++;
+        bytes32 digest = EIP712.hashMessage(
+            DOMAIN_SEPARATOR,
+            keccak256(abi.encode(VERIFY_VERYVERYCOMPLEX_TYPEHASH, owner, hashStruct(va), nonce))
+        );
+
+        address recovered = ecrecover(digest, v, r, s);
+        if (recovered == address(0) || recovered != owner) revert InvalidSignature(recovered);
+    }
+
+    function hashStruct(Receive calldata value) public view returns (bytes32) {
+        return
+            keccak256(
+                abi.encode(
+                    RECEIVE_TYPEHASH,
+                    keccak256(abi.encodePacked(value.receivers)),
+                    keccak256(abi.encodePacked(value.values))
+                )
+            );
+    }
+}
